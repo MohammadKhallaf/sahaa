7b8d495613aa72d33724f2ac6f6aab689d1d53de
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2024-11-05 16:01:23 +0200

    update file dependencies

--- src/components/navbar/index.test.tsx
+++ src/components/navbar/index.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { HashRouter } from "react-router-dom";
 import { describe, expect, it } from "vitest";
 import { Navbar } from ".";
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,6 +18,9 @@ export default defineConfig({
     }),
     tsconfigPaths(),
     react(),
+    checker({
+      typescript: true,
+    }),
   ],
 
   resolve: {
